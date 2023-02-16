30eb40efcd841235f386bb06042595fbdbd70a8f
Jirka <jirka.borovec@seznam.cz>
2023-02-16 14:28:51 +0100

    imports

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from rich import print
 from rich.logging import RichHandler
 
 from research_app.components.jupyter_notebook import JupyterLab
-from research_app.components.model_demo import ModelDemo
+from research_app.demo.model import ModelDemo
 from research_app.utils import clone_repo, notebook_to_html
 
 FORMAT = "%(message)s"
